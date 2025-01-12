12641ac00987eb799c4475ee7296e407835007a1
kais <121316071+kais-120@users.noreply.github.com>
2025-01-12 23:59:39 +0100

    add file auth

---
+++ Backend/app/Http/Controllers/useController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class useController extends Controller
+{
+    public function index(){
+        $user = User::all();
+        if(!$user->isEmpty()){
+            return response()->json(["data"=>$user,"status"=>200], 200);
+        }
+        return response()->json(["data"=>"Users are Empty","status"=>404], 404);
+
+
+    }
+}
