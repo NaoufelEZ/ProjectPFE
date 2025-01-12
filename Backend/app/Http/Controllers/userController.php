a1c2bbd66c30cf24a70acbf64dfb9252b3ab9e7d
kais <121316071+kais-120@users.noreply.github.com>
2025-01-12 09:21:46 +0100

    same modifie in userController

--- Backend/app/Http/Controllers/userController.php
+++ Backend/app/Http/Controllers/userController.php
@@ -7,9 +7,16 @@ use Illuminate\Http\Request;
 class userController extends Controller
 {
     public function store(Request $request){
-        $validUser = $request->validate(
-            [
+        $validUser = $request->validate([
                 "First_name"=>"required|String|min:30",
+                "Last_name"=>"required|String|min:30",
+                "Email"=>"required|email|unique",
+                "Password"=>"required|String|min:6",
+                "Phone"=>"required|String|min:30",
+                "Role"=>"required|String",
             ]);
+            if($validUser){
+                return response()->json(["data"=>"user add","status"=>"200"], 200);
+            }
     }
 }
