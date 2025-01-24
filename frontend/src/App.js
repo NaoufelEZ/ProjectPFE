85cdd133c8c13c0e9fda9219e2cb57a5f7a14c07
kais <121316071+kais-120@users.noreply.github.com>
2025-01-25 00:17:14 +0100

    add new Update in frontend an backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,24 @@ import Login from "./Auth/Login";
 import Register from "./Auth/Register";
 import Home from "./Home";
 import OtpVerfiy from "./Auth/otpVerfiy";
+import ReqAuth from "./Auth/ReqAuth";
+import Setting from "./Auth/Setting";
+import Profile from "./Auth/Profile";
+import Password from "./Auth/Password";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
+      <Route element={<ReqAuth />}>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Route>
       <Route path="/verify" element={<OtpVerfiy/>} />
+      <Route path="/setting" element={<Setting />}>
+        <Route path="profile" element={<Profile />}/>
+        <Route path="password" element={<Password />}/>
+      </Route>
     </Routes>
   );
 }
--- frontend/src/Component/Header.jsx
+++ frontend/src/Component/Header.jsx
@@ -1,19 +1,81 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import Cookies from "universal-cookie"
+import { ApiKey } from "../Api/Api";
+import { Dropdown } from "react-bootstrap";
+import "./headerStyle.css";
+import logo from "../Assit/images/logo.png";
 const Header = () => {
+  const [users,setUsers] = useState();
+  const cookie = new Cookies();
+  const user = cookie.get("auth");
+  const nav = useNavigate();
+  useEffect(()=>{
+    if(user){
+    axios.get("http://127.0.0.1:8000/api/v1/user",{
+      params:{
+        apiKey: ApiKey,
+      },
+      headers: {
+        Authorization: `Bearer ${user}`, 
+      },
+  }).then((data)=>setUsers(data.data.data)).catch((err)=>console.log(err))
+}
+  },[]);
+  console.log(users);
+  const handleLogout = ()=>{
+    axios.get("http://127.0.0.1:8000/api/v1/logout",{
+      params:{
+        apiKey: ApiKey,
+      },
+      headers: {
+        Authorization: `Bearer ${user}`, 
+      },
+    }).then(cookie.remove("auth"),nav("/"));
+  }
   return (
-    <header>
-        <nav>
+    <>
+    {users ? (users.email_verify === 0 ?
+    <div className="p-2 bg-light">
+      <p>you not verify your account</p>
+    </div> : "" )
+     : ""}
+    <header className="w-100 p-3">
+        <nav className="d-flex justify-content-between align-items-center">
             <div>
-                
+                <img src={logo} width={100} alt="logo"/>
             </div>
             <div>
-                
+                <ul className=" list-unstyled mx-3">
+                  <li>home</li>
+                  <li>about us</li>
+                  <li>contact</li>
+                </ul>
             </div>
+              {!user ?
             <div>
+              <Link className="p-3 text-decoration-none" to="/login">Login</Link>
+              <Link className="p-3 text-decoration-none" to="/register">Register</Link>
+            </div>
+              :  users ? 
+              <Dropdown className="d-inline mx-2 bg-transparent">
+              <Dropdown.Toggle id="dropdown-autoclose-true">
+              {/* {users.first_name} */}
 
-            </div>
+              <img className=" rounded-circle" width={50} src={`http://127.0.0.1:8000/images/avatars/${users.avatar}`} alt="prof"/>
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+            <Dropdown.Item href="/setting">Setting</Dropdown.Item>
+            <Dropdown.Item onClick={handleLogout} href="#">Logout</Dropdown.Item>
+            </Dropdown.Menu>
+            </Dropdown>
+            : "Loading..."
+              }
         </nav>
     </header>
+    </>
   )
 }
 
