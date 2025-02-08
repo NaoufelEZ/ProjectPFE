f1e34199ddfd68ad6e2d3c26bb49ce9c615dc6ff
kais <121316071+kais-120@users.noreply.github.com>
2025-02-08 12:57:20 +0100

    add dataBase of addresse and comment

---
+++ Backend/database/migrations/2025_02_05_193249_create_addresses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+            $table->string("states",30);
+            $table->string("address",100);
+            $table->string("zip",4);
+            $table->string("street",100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
