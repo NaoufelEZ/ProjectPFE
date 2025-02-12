a47aa3cf590a833ed383c7ec1baaf38525314b12
kais <121316071+kais-120@users.noreply.github.com>
2025-02-12 14:06:20 +0100

    wishlist table

---
+++ Backend/app/Http/Controllers/WishlistController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Wishlist;
+use Illuminate\Http\Request;
+
+class WishlistController extends Controller
+{
+    public function store(Request $request,$id) {
+        $user = $request->user();
+        $product = Product::find($id);
+        if(!$product){
+            return response()->json(["data"=>"Product Not Found","status"=>404], 404);
+        }
+        Wishlist::create([
+            "user_id"=>$user->id,
+            "product_id"=>$id,
+        ]);
+        return response()->json(["data"=>"Product Add TO Your List","status"=>200], 200);
+    }
+    public function index(Request $request) {
+        $user = $request->user()->id;
+        $wishlist = Wishlist::where("user_id",$user)->get();
+        if(!$wishlist){
+            return response()->json(["data"=>"You Wishlist Are Empty","status"=>404], 404);
+        }
+        return response()->json(["data"=>$wishlist,"status"=>200], 200);
+    }
+}
