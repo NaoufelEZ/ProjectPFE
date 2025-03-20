502a18cc4476e3acd906c5514df144815af41d24
kais <121316071+kais-120@users.noreply.github.com>
2025-03-20 16:55:16 +0100

    samethings new

--- Backend/database/seeders/SubcategoriesSeeder.php
+++ Backend/database/seeders/SubcategoriesSeeder.php
@@ -18,6 +18,8 @@ class SubcategoriesSeeder extends Seeder
             ["category_id"=>1,"subcategories"=>"Shoes","subcategories_image"=>"1741709322_67d0600ac664a.jpg"],
             ["category_id"=>2,"subcategories"=>"Clothes","subcategories_image"=>"1741710390_67d06436678c2.jpg"],
             ["category_id"=>2,"subcategories"=>"Shoes","subcategories_image"=>"1741710440_67d0646882d3a.jpg"],
+            ["category_id"=>1,"subcategories"=>"New","subcategories_image"=>"1741709376_67d0604068eac.jpg"],
+            ["category_id"=>2,"subcategories"=>"New","subcategories_image"=>"1741709322_67d0600ac664a.jpg"],
         ]);
     }
 }
